9f7884aba2581bc5a52142758385ddd2394a80c5
juliussiregar <juliussiregar1011@gmail.com>
2024-03-09 16:37:03 +0700

    update cv

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/julius.jpeg";
-import CV from "../../assets/John-Cv.pdf";
+import CV from "../../assets/CV-Julius-Siregar.pdf";
 import Info from "./Info";
 
 const About = () => {
