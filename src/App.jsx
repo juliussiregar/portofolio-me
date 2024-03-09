2604f85850047c60fb339e5e5222a770737e9d47
juliussiregar <juliussiregar1011@gmail.com>
2024-03-09 16:33:23 +0700

    update porto

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import './App.css'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
+import About from './components/about/about'
 
 const App = () => {
   return (
     <>
     <Header />
-    <main className='main'></main>
-      <Home />
+    <main className='main'>
+    <Home />
+    <About />
+    </main>
+     
     </>
 
   )
